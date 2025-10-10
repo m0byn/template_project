212213f5e9992864e0f34eac7396b929801f0366
Manuel Zerobin <manuel.zerobin@wifo.ac.at>
2025-10-10 17:54:04 +0200

    include rendering function instead of separate commands

--- R-Scripts/qmd_rendering.R
+++ R-Scripts/qmd_rendering.R
@@ -5,18 +5,26 @@
 ###############################################################################################################################
 
 #  ---------------------------------------Standard rendering of quarto files--------------------------------------------
-# All rendering
-system("quarto render K:/Projects/PROJECTNAME/Quarto-Files/FILENAME.qmd --output-dir K:/Projects/PROJECTNAME/Quarto-Files/Rendered/FILENAME/all"); unlink("K:/Projects/PROJECTNAME/Quarto-Files/FILENAME_files", recursive = TRUE)
 
-#  ---------------------------------------Gitea rendering of quarto files----------------------------------------------
-wifo.base::wbQuartoKnitAndTOC(file.path('/Quarto_files/Test.qmd'))
+# Rendering
 
-#  ---------------------------------------Separate rendering of quarto files--------------------------------------------
-# HTML rendering
-system("quarto render K:/Projects/OeNB-PuG/Quarto_files/MergeData.qmd --to html --output-dir K:/Projects/OeNB-PuG/Quarto_files/Rendered/MergeData/html"); unlink("K:/Projects/OeNB-PuG/Quarto_files/MergeData_files", recursive = TRUE)
-# PDF rendering
-system("quarto render K:/Projects/OeNB-PuG/Quarto_files/MergeData.qmd --to pdf --output-dir K:/Projects/OeNB-PuG/Quarto_files/Rendered/MergeData/pdf"); unlink("K:/Projects/OeNB-PuG/Quarto_files/MergeData_files", recursive = TRUE)
-# Word rendering
-system("quarto render K:/Projects/OeNB-PuG/Quarto_files/MergeData.qmd --to docx --output-dir K:/Projects/OeNB-PuG/Quarto_files/Rendered/MergeData/docx"); unlink("K:/Projects/OeNB-PuG/Quarto_files/MergeData_files", recursive = TRUE)
-# Commonmark rendering
-system("quarto render K:/Projects/OeNB-PuG/Quarto_files/MergeData.qmd --to commonmark --output-dir K:/Projects/OeNB-PuG/Quarto_files/Rendered/MergeData/commonmark"); unlink("K:/Projects/OeNB-PuG/Quarto_files/MergeData_files", recursive = TRUE)
+render = function(proj.name, qmd.name) {
+  # Construct file paths
+  qmd.path = file.path("K:/Projects", proj.name, "Quarto-Files", paste0(qmd.name, ".qmd"))
+  output.dir = file.path("K:/Projects", proj.name, "Quarto-Files/Rendered", qmd.name)
+  files.dir = file.path("K:/Projects", proj.name, "Quarto-Files", paste0(qmd.name, "_files"))
+  gitignore.path = file.path("K:/Projects", proj.name, "Quarto-Files", ".gitignore")
+  
+  # Render command
+  render.cmd = paste0("quarto render ", qmd.path, " --output-dir ", output.dir)
+  
+  # Execute commands
+  system(render.cmd)
+  unlink(files.dir, recursive = TRUE)
+  unlink(gitignore.path)
+  
+  cat("Rendered:", qmd.name, "for project:", proj.name, "\n")
+}
+
+
+render('BL-Healthcare', 'DataPreparation')
